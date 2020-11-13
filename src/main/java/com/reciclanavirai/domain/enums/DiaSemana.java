b9f3e1320a88849cb4f325a8d8c4de5d80c34ff5
marcosrogerio.jrf <marcosrogerio.jrf@gmail.com>
2020-11-12 20:09:14 -0400

    Atividade - Alteração de JsonProperty

--- src/main/java/com/reciclanavirai/domain/enums/DiaSemana.java
+++ src/main/java/com/reciclanavirai/domain/enums/DiaSemana.java
@@ -1,17 +1,28 @@
 package com.reciclanavirai.domain.enums;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@JsonDeserialize
 public enum DiaSemana {
+	@JsonProperty("Domingo")
 	DOMINGO(1, "Domingo"),
+	@JsonProperty("Segunda-feira")
 	SEGUNDA(2, "Segunda-feira"),
+	@JsonProperty("Terça-feira")
 	TERCA(3, "Terça-feira"),
+	@JsonProperty("Quarta-feira")
 	QUARTA(4, "Quarta-feira"),
+	@JsonProperty("Quinta-feira")
 	QUINTA(5, "Quinta-feira"),
+	@JsonProperty("Sexta-feira")
 	SEXTA(6, "Sexta-feira"),
+	@JsonProperty("Sábado")
 	SABADO(7, "Sábado");
 	
 	private int id;
--- src/main/java/com/reciclanavirai/domain/enums/TipoColeta.java
+++ src/main/java/com/reciclanavirai/domain/enums/TipoColeta.java
@@ -1,12 +1,16 @@
 package com.reciclanavirai.domain.enums;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum TipoColeta {
+	@JsonProperty("Coleta Seletiva")
 	COLETASELETIVA(1, "Coleta Seletiva"),
+	@JsonProperty("Coleta Comum")
 	COLETACOMUM(2, "Coleta Comum");
 		
 	private int id;
